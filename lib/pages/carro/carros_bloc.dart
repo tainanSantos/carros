c28860c9dc8723d412d4e59e28e1f844621eb376
tainanSantos <tainan206@gmail.com>
2021-01-26 14:59:57 -0300

    refectori carroBloc

--- lib/pages/carro/carros_bloc.dart
+++ lib/pages/carro/carros_bloc.dart
@@ -7,7 +7,7 @@ import 'package:carros/pages/carro/carros_api.dart';
 class CarrosBloc {
   final _streamController = StreamController<List<Carro>>();
 
-  Stream<List<Carro>> get stream => _streamController.Stream;
+  Stream<List<Carro>> get stream => _streamController.stream;
 
   fetch(String tipo) async {
     List<Carro> carros = await CarrosApi.getCarros(tipo);
