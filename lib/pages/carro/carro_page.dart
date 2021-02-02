0ed9f9ca23049d4667b2c47a2872f689718802b8
tainanSantos <tainan206@gmail.com>
2021-02-02 09:16:30 -0300

    layout carro_page: descriçao do carro mais texto loren

--- lib/pages/carro/carro_page.dart
+++ lib/pages/carro/carro_page.dart
@@ -51,46 +51,69 @@ class CarroPage extends StatelessWidget {
         children: <Widget>[
           Image.network(carro?.urlFoto ??
               "https://img.olx.com.br/images/61/616098458720941.jpg"),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  text(
-                    carro.nome,
-                    fontSize: 20,
-                    bold: true,
-                  ),
-                  text(
-                    carro.tipo,
-                    fontSize: 16,
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    icon: Icon(
-                      Icons.favorite,
-                      color: Colors.red,
-                      size: 35,
-                    ),
-                    onPressed: _onClickFavorito,
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.share,
-                      size: 35,
-                    ),
-                    onPressed: _onClickShare,
-                  ),
-                ],
-              )
-            ],
-          )
+          _bloco1(),
+          Divider(),
+          _bloco2()
         ],
       ),
+    );
+  }
+
+  Row _bloco1() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            text(
+              carro.nome,
+              fontSize: 20,
+              bold: true,
+            ),
+            text(
+              carro.tipo,
+              fontSize: 16,
+            )
+          ],
+        ),
+        Row(
+          children: [
+            IconButton(
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.red,
+                size: 35,
+              ),
+              onPressed: _onClickFavorito,
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.share,
+                size: 35,
+              ),
+              onPressed: _onClickShare,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  _bloco2() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        text(carro.descricao, fontSize: 16, bold: true),
+        SizedBox(
+          height: 20,
+        ),
+        text(
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+      ],
     );
   }
 
